bc3b0f658a9a70d6920ef8adaef19e40f8f385b0
Andrey Belyaev <ianbel263@gmail.com>
2022-01-07 22:04:51 +0300

    update

--- prepare_docker.sh
+++ prepare_docker.sh
@@ -7,7 +7,11 @@ docker volume prune -f
 docker network prune -f
 docker system prune -f
 
-docker network create nginx-proxy
+docker network create \
+  --driver=bridge \
+  --subnet=172.29.0.0/16 \
+  --gateway=172.29.0.1 \
+  nginx-proxy
 docker volume create static
 docker volume create media
 docker-compose -f /home/ianbel/geekshop/docker-compose.yml up --build -d
