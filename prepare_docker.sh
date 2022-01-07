f9f8da00c64c4c1d38f5402831f59c7511dcb884
Andrey Belyaev <ianbel263@gmail.com>
2022-01-07 11:51:07 +0300

    update

--- prepare_docker.sh
+++ prepare_docker.sh
@@ -3,6 +3,10 @@
 sudo chown ianbel:ianbel -R ./nginx-proxy/data
 
 docker docker stop $(docker ps -a -q)
+docker volume prune -f
+docker network prune -f
+docker system prune -f
+
 docker network create nginx-proxy
 docker volume create static
 docker volume create media
